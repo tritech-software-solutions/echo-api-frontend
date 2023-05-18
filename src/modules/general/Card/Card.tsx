dc87724f2ca70f5f5ff7e3d8841e70bb8bca7f8b
Gareth Fultz <garethfultz@gmail.com>
2023-05-18 18:54:10 -0400

    Card & Home Example

---
+++ src/modules/general/Card/Card.tsx
@@ -0,0 +1,16 @@
+import "./Card.css";
+
+import React from "react";
+
+type Props = {
+    children?: React.ReactNode;
+    className?: string;
+};
+
+class Card extends React.Component<Props> {
+    render() {
+        return <div className={"card" + (this.props.className ? " " + this.props.className : "")}>{this.props.children}</div>;
+    }
+}
+
+export default Card;
--- src/modules/pages/home/HomePage.tsx
+++ src/modules/pages/home/HomePage.tsx
@@ -1,10 +1,28 @@
+import Card from "../../general/Card/Card";
 import "./HomePage.css";
 
 import React from "react";
 
 class HomePage extends React.Component {
     render() {
-        return <div className="">Home Page Here</div>;
+        return (
+            <div>
+                <div>
+                    <Card className="topLeft">
+                        <h1>Create your first API simulator</h1>
+                        ...
+                    </Card>
+                    <Card className="bottomLeft">
+                        <h1>View Your Current</h1>
+                        ...
+                    </Card>
+                </div>
+                <Card className="bottomLeft">
+                    <h1>Get insights</h1>
+                    ...
+                </Card>
+            </div>
+        );
     }
 }
 
