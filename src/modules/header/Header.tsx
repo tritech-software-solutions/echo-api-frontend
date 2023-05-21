972cd8f6bafe33fe02503ed63749748a4040b34e
Nate-Louder <nmlouder@gmail.com>
2023-05-21 13:20:47 -0400

    Added a simple header component #2

--- src/modules/header/Header.tsx
+++ src/modules/header/Header.tsx
@@ -3,12 +3,27 @@ import "./Header.css";
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+
+
 class Header extends React.Component {
     render() {
         return (
             <div className="header">
                 <Link to="/" className="noSelect noDecoration headerTitle">
-                    Echo Api
+                    <Logo className="logo"/>
+                </Link>
+                <Link to="/" className="noSelect noDecoration headerTitle">
+                    <div className="text">Build API Sim</div>
+                </Link>
+                <Link to="/" className="noSelect noDecoration headerTitle">
+                    <div className="text">My API Sims</div>
+                </Link>
+                <Link to="/" className="noSelect noDecoration headerTitle">
+                    <div className="text">Analytics</div>
+                </Link>
+                <Link to="/" className="noSelect noDecoration headerTitle">
+                    <div className="text">Settings</div>
                 </Link>
             </div>
         );
--- src/modules/pages/home/HomePage.tsx
+++ src/modules/pages/home/HomePage.tsx
@@ -10,6 +10,7 @@ import { ReactComponent as PlusSvg } from '../../../assets/icons/plus-solid.svg'
 import GraphSvg from '../../../assets/graph.png';
 
 import Button from "../../general/Button/Button";
+import { Link } from "react-router-dom";
 
 class HomePage extends React.Component {
     
@@ -26,41 +27,47 @@ class HomePage extends React.Component {
                 <div className="home-cards">
                     <div className="left-cards">
                         <Card>
-                            <h1 className="header">Create Your First API Simulator</h1>
+                            <h1 className="card-header">Create Your First API Simulator</h1>
                             <p className="description">
                                 Create a locally hosted API that can be used for testing your application. Simulate any existing API without the fear of making too many unnecessary calls, or dealing with changing data. 
                             </p>
-                            <Button
-                                text={this.buttonText[0]}
-                                hasText={true}
-                                icon={<PlusSvg className="plus-icon"/>} 
-                            />
+                            <Link to="/" className="noSelect noDecoration headerTitle">
+                                <Button
+                                    text={this.buttonText[0]}
+                                    hasText={true}
+                                    icon={<PlusSvg className="plus-icon"/>} 
+                                />
+                            </Link>
                         </Card>
                         <Card>
-                            <h1 className="header">View Your Current API Simulators</h1>
+                            <h1 className="card-header">View Your Current API Simulators</h1>
                             <p className="description">
                                 View your created API simulators. Run for testing or make changes to your existing setups.
                             </p>
-                            <Button 
-                                text={this.buttonText[1]}
-                                hasText={true} 
-                                icon={<EyeSvg className="eye-icon"/>}
-                                />
+                            <Link to="/" className="noSelect noDecoration headerTitle">
+                                <Button 
+                                    text={this.buttonText[1]}
+                                    hasText={true} 
+                                    icon={<EyeSvg className="eye-icon"/>}
+                                    />
+                            </Link>
                         </Card>
                     </div>
                     <Card>
-                        <h1 className="header right-card">Get Insight Into How Your App Uses Your APIs</h1>
+                        <h1 className="card-header right-card">Get Insight Into How Your App Uses Your APIs</h1>
                         <div className="graph-svg">
                             <img src={GraphSvg} alt="GraphSvg" className="image"/>
                         </div>
                         <p className="description right-card">
                             Analytics are a window into how your application is interacting with the API without actually using the API. Gain critical insights into what endpoints your app is calling and how often it is making those calls. Great for debugging and verifying that your application is performing tasks as expected.
                         </p>
-                        <Button 
-                            text={this.buttonText[2]} 
-                            hasText={true}
-                            icon={<ChartSvg className="chart-icon"/>}
-                        />
+                        <Link to="/" className="noSelect noDecoration headerTitle">
+                            <Button 
+                                text={this.buttonText[2]} 
+                                hasText={true}
+                                icon={<ChartSvg className="chart-icon"/>}
+                            />
+                        </Link>
                     </Card>
                 </div>
             </div>
