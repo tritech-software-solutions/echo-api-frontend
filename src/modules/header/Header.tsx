3c7234cc14bf067a826df00f3b3458f9e0bcb92e
Nate-Louder <nmlouder@gmail.com>
2023-05-23 21:48:50 -0400

    Created parent input class

--- src/modules/header/Header.tsx
+++ src/modules/header/Header.tsx
@@ -5,74 +5,62 @@ import { Link } from "react-router-dom";
 
 import { ReactComponent as Logo } from "../../assets/logo.svg";
 
-
 type State = {
     activeLink: string;
-}
+};
 
-type Props = {}
+type Props = {};
 
 class Header extends React.Component<Props, State> {
-    
     state = {
-        activeLink: "/"
-    }
-  
-    handleClick = (link: string) => {
-      this.setState({ activeLink: link });
+        activeLink: "/",
     };
-  
+
+    handleClick = (link: string) => {
+        this.setState({ activeLink: link });
+    };
+
     render() {
-      const { activeLink } = this.state;
-  
-      return (
-        <div className="header">
-          <Link
-            to="/"
-            className={`noSelect noDecoration headerTitle `}
-            onClick={() => this.handleClick("/")}
-          >
-            <Logo className="logo" />
-          </Link>
-          <Link
-            to="/endpoint"
-            className={`noSelect noDecoration headerTitle ${
-              activeLink === "/endpoint" ? "active" : ""
-            }`}
-            onClick={() => this.handleClick("/endpoint")}
-          >
-            <div className="text">Build API Sim</div>
-          </Link>
-          <Link
-            to="/view"
-            className={`noSelect noDecoration headerTitle ${
-              activeLink === "/view" ? "active" : ""
-            }`}
-            onClick={() => this.handleClick("/view")}
-          >
-            <div className="text">My API Sims</div>
-          </Link>
-          <Link
-            to="/analytics"
-            className={`noSelect noDecoration headerTitle ${
-              activeLink === "/analytics" ? "active" : ""
-            }`}
-            onClick={() => this.handleClick("/analytics")}
-          >
-            <div className="text">Analytics</div>
-          </Link>
-          <Link
-            to="/settings"
-            className={`noSelect noDecoration headerTitle ${
-              activeLink === "/settings" ? "active" : ""
-            }`}
-            onClick={() => this.handleClick("/settings")}
-          >
-            <div className="text">Settings</div>
-          </Link>
-        </div>
-      );
+        const { activeLink } = this.state;
+
+        return (
+            <div className="header">
+                <Link to="/" className={`noSelect noDecoration headerTitle `} onClick={() => this.handleClick("/")}>
+                    <Logo className="logo" />
+                </Link>
+                <div className="text-links">
+                    <Link
+                        to="/endpoint"
+                        className={`noSelect noDecoration headerTitle ${activeLink === "/endpoint" ? "active" : ""}`}
+                        onClick={() => this.handleClick("/endpoint")}
+                    >
+                        <div className="text">Build API Sim</div>
+                    </Link>
+                    <Link
+                        to="/view"
+                        className={`noSelect noDecoration headerTitle ${activeLink === "/view" ? "active" : ""}`}
+                        onClick={() => this.handleClick("/view")}
+                    >
+                        <div className="text">My API Sims</div>
+                    </Link>
+                    <Link
+                        to="/analytics"
+                        className={`noSelect noDecoration headerTitle ${activeLink === "/analytics" ? "active" : ""}`}
+                        onClick={() => this.handleClick("/analytics")}
+                    >
+                        <div className="text">Analytics</div>
+                    </Link>
+                    <Link
+                        to="/settings"
+                        className={`noSelect noDecoration headerTitle ${activeLink === "/settings" ? "active" : ""}`}
+                        onClick={() => this.handleClick("/settings")}
+                    >
+                        <div className="text">Settings</div>
+                    </Link>
+                </div>
+            </div>
+        );
     }
-  }
-  
-  export default Header;
+}
+
+export default Header;
--- src/modules/pages/endpointViewer/EndpointViewer.tsx
+++ src/modules/pages/endpointViewer/EndpointViewer.tsx
@@ -3,11 +3,13 @@ import "./EndpointViewer.css";
 import Card from "../../general/Card/Card";
 import React from "react";
 import Collapsible from "../../general/Collapsible/Collapsible";
+import InputField from "../../general/inputFields/InputField";
+import LongInput from "../../general/inputFields/Long";
 
 class EndpointViewer extends React.Component {
     render() {
         return (
-            <div>Viewer Here</div>
+            <LongInput name="Test" />
         );
     }
 }
