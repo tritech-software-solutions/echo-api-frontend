4326302e2c66628e3c45b5391f1115a0e098fddb
Nate-Louder <nmlouder@gmail.com>
2023-06-01 06:39:18 -0400

    Began work on theme #9

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,74 @@ import EndpointEditor from "./modules/pages/endpointEditor/EndpointEditor";
 import EndpointViewer from "./modules/pages/endpointViewer/EndpointViewer";
 import Settings from "./modules/pages/settings/Settings";
 import Analytics from "./modules/pages/analytics/Analytics";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const lightTheme = createTheme({
+    palette: {
+        mode: "light",
+        primary: {
+            main: "#3b3c8d",
+            light: "#494da2",
+            dark: "#27216b",
+            contrastText: "#FFFFFF",
+        },
+        secondary: {
+            main: "#1bb3a9",
+            light: "#a9deda",
+            dark: "#009283",
+            contrastText: "#FFFFFF",
+        },
+        error: {
+            main: "#db3633",
+            light: "#e6504d",
+            dark: "#bc2626",
+            contrastText: "#FFFFFF",
+        },
+        success: {
+            main: "#26bc26",
+            light: "#76d170",
+            dark: "#009a0a",
+            contrastText: "#FFFFFF",
+        },
+        neutral: {
+            main: "#64748B",
+            contrastText: "#FFFFFF",
+        },
+    },
+    typography: {
+        fontFamily: "Roboto, Jaldi, Viga",
+        h1: {},
+    },
+});
+
+declare module "@mui/material/styles" {
+    interface Palette {
+        neutral: Palette["primary"];
+    }
+
+    interface PaletteOptions {
+        neutral?: PaletteOptions["primary"];
+    }
+}
+
+const darkTheme = createTheme({
+    palette: {
+        mode: "dark",
+        primary: {
+            main: "#3b3c8d",
+            light: "#494da2",
+            dark: "#27216b",
+            contrastText: "#FFFFFF",
+        },
+        secondary: {
+            main: "#1bb3a9",
+            light: "#a9deda",
+            dark: "#009283",
+            contrastText: "#FFFFFF",
+        },
+    },
+});
 
 function App() {
     return (
