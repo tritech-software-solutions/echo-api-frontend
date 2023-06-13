89d1af57287d3f6749d90455b3a7144fc6a0be86
Gareth Fultz <garethfultz@gmail.com>
2023-06-12 20:33:40 -0400

    Improved Parameter Support
    Co-authored-by: Louder, Nathaniel (loudernm) <loudernm@mail.uc.edu>

--- src/modules/pages/endpointEditor/EndpointEditor.tsx
+++ src/modules/pages/endpointEditor/EndpointEditor.tsx
@@ -1,7 +1,8 @@
 import "./EndpointEditor.css";
 
-import React from "react";
+import React, { ChangeEvent } from "react";
 import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 import EditIcon from "@mui/icons-material/Edit";
 import {
     Accordion,
@@ -22,8 +23,21 @@ import {
     Stack,
     styled,
     IconButton,
+    SelectChangeEvent,
 } from "@mui/material";
 import { BoxProps, Container, StackProps } from "@mui/system";
+
+type EndpointParameter = {
+    name: string;
+    value: string;
+    type: EndpointParameterType;
+};
+
+enum EndpointParameterType {
+    Any,
+    Number,
+    Boolean,
+}
 
 type Props = {};
 
@@ -31,6 +45,7 @@ type State = {
     isEditingTitle: boolean;
     name: string;
     endpoint: string;
+    parameters: EndpointParameter[];
 };
 
 const TitleRowStack = styled((props: StackProps) => (
@@ -41,6 +56,7 @@ class EndpointEditor extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
 
+        this.addParameter = this.addParameter.bind(this);
         this.handleNewTitle = this.handleNewTitle.bind(this);
         this.handleNameChange = this.handleNameChange.bind(this);
         this.handleEndpointChange = this.handleEndpointChange.bind(this);
@@ -49,6 +65,7 @@ class EndpointEditor extends React.Component<Props, State> {
             isEditingTitle: false,
             name: "Example",
             endpoint: "Endpoint",
+            parameters: [],
         };
     }
 
@@ -57,72 +74,98 @@ class EndpointEditor extends React.Component<Props, State> {
             <Card className="endpoint-editor-card">
                 <CardContent>
                     <Stack divider={<Divider orientation="vertical" flexItem />} spacing={1}>
-                        {this.state.isEditingTitle ? (
-                            <TitleRowStack>
-                                <TextField
-                                    id="name-input"
-                                    label="Name"
-                                    variant="outlined"
-                                    required
-                                    onBlur={this.handleNameChange}
-                                    defaultValue={this.state.name}
-                                />
-                                <TextField
-                                    id="endpoint-input"
-                                    label="Endpoint"
-                                    variant="outlined"
-                                    required
-                                    onBlur={this.handleEndpointChange}
-                                    defaultValue={this.state.endpoint}
-                                />
-                                <Button
-                                    color="secondary"
-                                    variant="contained"
-                                    sx={{ margin: "auto 0" }}
-                                    onFocus={this.handleNewTitle}
-                                >
-                                    Submit
-                                </Button>
-                            </TitleRowStack>
-                        ) : (
-                            <TitleRowStack spacing={0}>
-                                <Typography variant="h1" color="primary">
-                                    {this.state.name} | {this.state.endpoint}
-                                </Typography>
-                                <Box>
-                                    <IconButton aria-label="edit" onFocus={this.handleNewTitle}>
-                                        <EditIcon />
-                                    </IconButton>
-                                </Box>
-                            </TitleRowStack>
-                        )}
+                        <Stack direction="row" justifyContent="space-between">
+                            {this.state.isEditingTitle ? (
+                                <TitleRowStack>
+                                    <TextField
+                                        id="name-input"
+                                        label="Name"
+                                        variant="outlined"
+                                        required
+                                        onBlur={this.handleNameChange}
+                                        defaultValue={this.state.name}
+                                    />
+                                    <TextField
+                                        id="endpoint-input"
+                                        label="Endpoint"
+                                        variant="outlined"
+                                        required
+                                        onBlur={this.handleEndpointChange}
+                                        defaultValue={this.state.endpoint}
+                                    />
+                                    <Button
+                                        color="secondary"
+                                        variant="contained"
+                                        sx={{ margin: "auto 0" }}
+                                        onFocus={this.handleNewTitle}
+                                    >
+                                        Submit
+                                    </Button>
+                                </TitleRowStack>
+                            ) : (
+                                <TitleRowStack>
+                                    <Typography variant="h1" color="primary">
+                                        {this.state.name} | {this.state.endpoint}
+                                    </Typography>
+                                    <Box>
+                                        <IconButton aria-label="edit" onFocus={this.handleNewTitle}>
+                                            <EditIcon />
+                                        </IconButton>
+                                    </Box>
+                                </TitleRowStack>
+                            )}
+                            <Box mt="auto" mb="auto" pr={4}>
+                                <Button size="large">Headers</Button>
+                            </Box>
+                        </Stack>
 
                         <Stack spacing={1}>
                             <TitleRowStack>
                                 <Typography variant="h3" color="primary">
                                     Parameters
                                 </Typography>
-                                <IconButton>
+                                <IconButton onClick={this.addParameter}>
                                     <AddIcon />
                                 </IconButton>
                             </TitleRowStack>
-                            <Stack spacing={1} direction={"row"}>
-                                <TextField id="param1-name-input" label="Name" variant="outlined" />
-                                <TextField id="param1-value-input" label="Value" variant="outlined" />
-                                <FormControl>
-                                    <InputLabel id="param1-type-input-label">Type</InputLabel>
-                                    <Select
-                                        labelId="param1-type-input-label"
-                                        id="param1-type-input"
-                                        value={undefined}
-                                        label="Type"
-                                    >
-                                        <MenuItem value={"any"}>Any</MenuItem>
-                                        <MenuItem value={"number"}>Number</MenuItem>
-                                        <MenuItem value={"boolean"}>Boolean</MenuItem>
-                                    </Select>
-                                </FormControl>
-                            </Stack>
+
+                            {this.state.parameters.map((value, index) => {
+                                return (
+                                    <Stack spacing={1} direction={"row"}>
+                                        <TextField
+                                            id={`param-name-input-${index}`}
+                                            label="Name"
+                                            variant="outlined"
+                                            value={this.state.parameters[index].name}
+                                            onChange={this.updateParameterName.bind(this, index)}
+                                        />
+                                        <TextField
+                                            id={`param-value-input-${index}`}
+                                            label="Value"
+                                            variant="outlined"
+                                            value={this.state.parameters[index].value}
+                                            onChange={this.updateParameterValue.bind(this, index)}
+                                        />
+                                        <FormControl>
+                                            <InputLabel id={`param-type-input-label-${index}`}>Type</InputLabel>
+                                            <Select
+                                                labelId={`param-type-input-label-${index}`}
+                                                id={`param-type-input-${index}`}
+                                                value={this.state.parameters[index].type}
+                                                label="Type"
+                                                onChange={this.updateParameterType.bind(this, index)}
+                                            >
+                                                <MenuItem value={EndpointParameterType.Any}>Any</MenuItem>
+                                                <MenuItem value={EndpointParameterType.Number}>Number</MenuItem>
+                                                <MenuItem value={EndpointParameterType.Boolean}>Boolean</MenuItem>
+                                            </Select>
+                                        </FormControl>
+                                        <IconButton onClick={this.removeParameter.bind(this, index)}>
+                                            <RemoveIcon />
+                                        </IconButton>
+                                    </Stack>
+                                );
+                            })}
                         </Stack>
 
                         <Stack spacing={1}>
@@ -132,7 +175,7 @@ class EndpointEditor extends React.Component<Props, State> {
                             <Stack spacing={1} direction={"row"}>
                                 <TextField id="outlined-number" label="Status" type="number" sx={{ maxWidth: "15em" }} />
                             </Stack>
-                            <TextField id="response-content" label="Multiline" multiline rows={8} defaultValue="" />
+                            <TextField id="response-content" label="Body" multiline rows={8} defaultValue="" />
                         </Stack>
                     </Stack>
                 </CardContent>
@@ -158,7 +201,40 @@ class EndpointEditor extends React.Component<Props, State> {
         });
     }
 
-    addParamter() {}
+    // #region parameters
+
+    addParameter() {
+        const newParams = this.state.parameters.slice();
+        newParams.push({
+            name: "",
+            value: "",
+            type: EndpointParameterType.Any,
+        });
+        this.setState({ parameters: newParams });
+    }
+
+    removeParameter(index: number) {
+        const newParams = this.state.parameters.slice();
+        newParams.splice(index, 1);
+        this.setState({ parameters: newParams });
+    }
+
+    updateParameterName(index: number, evt: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        this.state.parameters[index].name = evt.target.value;
+        this.setState({ parameters: this.state.parameters });
+    }
+
+    updateParameterValue(index: number, evt: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        this.state.parameters[index].value = evt.target.value;
+        this.setState({ parameters: this.state.parameters });
+    }
+
+    updateParameterType(index: number, evt: SelectChangeEvent<EndpointParameterType>) {
+        this.state.parameters[index].type = evt.target.value as EndpointParameterType;
+        this.setState({ parameters: this.state.parameters });
+    }
+
+    // #endregion
 }
 
 export default EndpointEditor;
