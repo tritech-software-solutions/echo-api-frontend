0de3b3b54be57edb08a87e1e6e8bced80f1706f9
Nate-Louder <nmlouder@gmail.com>
2023-08-12 11:51:59 -0400

    No clue what is going on

--- src/modules/header/Header.tsx
+++ src/modules/header/Header.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import { ReactComponent as Logo } from "../../assets/logo.svg";
-import { Box, Tab, Tabs, AppBar, Container, Toolbar, Typography, IconButton } from "@mui/material";
+import { Box, Tab, Tabs, AppBar, Container, Toolbar, Typography, IconButton, Button } from "@mui/material";
 import { faAlignCenter } from "@fortawesome/free-solid-svg-icons";
 import { Stack, margin, padding, textAlign } from "@mui/system";
 
@@ -35,46 +35,47 @@ class Header extends React.Component<Props, State> {
         const { activeLink } = this.state;
 
         return (
-            <Box
-                sx={{ flexGrow: 1, marginLeft: "auto", marginRight: "auto", marginBottom: "2em", marginTop: "1em" }}
+            <Stack
+                sx={{ width: "100%", marginTop: "2em", marginBottom: "3em", marginLeft: "auto", marginRight: "auto" }}
                 maxWidth={this.state.headerSize}
             >
-                <AppBar sx={{ background: "transparent" }} elevation={0} position="static">
-                    <Stack sx={{ flexGrow: 1, justifyContent: "space-between", flexDirection: "row", alignItems: "center" }}>
-                        <Link to="/" className={`noSelect noDecoration headerTitle `} onClick={() => this.handleClick("/")}>
-                            <Logo className="logo" />
-                        </Link>
-                        <Link
-                            to="/endpoint"
-                            className={`noSelect noDecoration headerTitle ${activeLink === "/endpoint" ? "active" : ""}`}
-                            onClick={() => this.handleClick("/endpoint")}
-                        >
-                            <div className="text">Build API Sim</div>
-                        </Link>
-                        <Link
-                            to="/view"
-                            className={`noSelect noDecoration headerTitle ${activeLink === "/view" ? "active" : ""}`}
-                            onClick={() => this.handleClick("/view")}
-                        >
-                            <div className="text">My API Sims</div>
-                        </Link>
-                        <Link
-                            to="/analytics"
-                            className={`noSelect noDecoration headerTitle ${activeLink === "/analytics" ? "active" : ""}`}
-                            onClick={() => this.handleClick("/analytics")}
-                        >
-                            <div className="text">Analytics</div>
-                        </Link>
-                        <Link
-                            to="/settings"
-                            className={`noSelect noDecoration headerTitle ${activeLink === "/settings" ? "active" : ""}`}
-                            onClick={() => this.handleClick("/settings")}
-                        >
-                            <div className="text">Settings</div>
-                        </Link>
-                    </Stack>
-                </AppBar>
-            </Box>
+                <Stack sx={{ flexGrow: 1, justifyContent: "center", flexDirection: "row", alignItems: "center", gap: "2em" }}>
+                    <Link to="/" className={`noSelect noDecoration headerTitle `} onClick={() => this.handleClick("/")}>
+                        <Logo className="logo" />
+                    </Link>
+                    <Link
+                        to="/endpoint"
+                        className={`noSelect noDecoration headerTitle ${activeLink === "/endpoint" ? "active" : ""}`}
+                        onClick={() => this.handleClick("/endpoint")}
+                    >
+                        <div className="text">Build API Sim</div>
+                    </Link>
+                    <Link
+                        to="/view"
+                        className={`noSelect noDecoration headerTitle ${activeLink === "/view" ? "active" : ""}`}
+                        onClick={() => this.handleClick("/view")}
+                    >
+                        <div className="text">My API Sims</div>
+                    </Link>
+                    <Link
+                        to="/analytics"
+                        className={`noSelect noDecoration headerTitle ${activeLink === "/analytics" ? "active" : ""}`}
+                        onClick={() => this.handleClick("/analytics")}
+                    >
+                        <div className="text">Analytics</div>
+                    </Link>
+                    <Link
+                        to="/settings"
+                        className={`noSelect noDecoration headerTitle ${activeLink === "/settings" ? "active" : ""}`}
+                        onClick={() => this.handleClick("/settings")}
+                    >
+                        <div className="text">Settings</div>
+                    </Link>
+                </Stack>
+                <Stack sx={{ marginRight: "1em" }}>
+                    <Button>Hello</Button>
+                </Stack>
+            </Stack>
         );
     }
 }
